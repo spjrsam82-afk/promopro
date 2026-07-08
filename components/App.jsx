0c1c91fef07515273ff11e44405b05612db61fc7
Samuel E Purcell <spjrsam82@gmail.com>
2026-07-08 11:43:53 -0700

    Update App.jsx
    Updated

--- components/App.jsx
+++ components/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import SeoContent from "./components/SeoContent";
 import Footer from "./components/Footer";
-import PromoFooter from "./components/App.jsx/PromoFooter";
+import PromoFooter from "./components/PromoFooter";
 
 function App() {
   return (
